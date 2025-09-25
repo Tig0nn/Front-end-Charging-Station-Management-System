8df4c90376ce26b2e73b9d66907369b223f05854
HuyNVT <huybearco123@gmail.com>
2025-09-25 13:01:25 +0700

    Add user information page

---
+++ src/AddUserInfoPage.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./AddUserInfoPage.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+function HandleClick(e) {
+    e.preventDefault();
+    alert("Form đã được submit!");
+}
+
+function AddUserInfoForm() {
+    return (
+        <div className="Background">
+            <div className="container">
+                <Form className="form-container" onSubmit={HandleClick}>
+                    <div className="title">
+                        <h1>Vui lòng nhập thông tin</h1>
+                    </div>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Tên người dùng</Form.Label>
+                        <Form.Control className="placeholdertxt" type="fullname" placeholder="Nguyễn Văn A" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicGender">
+                        <Form.Label>Giới tính</Form.Label>
+                        <div>
+                            <Form.Check className="form-radio" inline label="Nam" name="gender" type="radio" id="gender-nam" value="Nam" defaultChecked/*mặc định để là nam*//>
+                            <Form.Check className="form-radio" inline label="Nữ"name="gender" type="radio" id="gender-nu" value="Nữ" />
+                        </div>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Số điện thoại</Form.Label>
+                        <Form.Control className="placeholdertxt" type="phoneNumber" placeholder="+84-XXX-XXX-XXX" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control className="placeholdertxt" type="email" placeholder="example123@gmail.com" />
+                    </Form.Group>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" />
+                        <label class="form-check-label">Tôi xin cam đoan mọi thông tin trên đều chuẩn xác.</label>
+                    </div>
+
+                    <Button variant="primary" type="submit">
+                        Xác nhận
+                    </Button>
+                </Form>
+            </div></div>
+    );
+}
+
+export default AddUserInfoForm;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import AddUserInfoForm from "./AddUserInfoPage.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <AddUserInfoForm/>
 );
