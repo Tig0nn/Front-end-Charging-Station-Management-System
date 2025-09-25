5f6098bc35d1b8d2c725e459194be556c6c13df1
HuyNVT <huybearco123@gmail.com>
2025-09-25 11:48:39 +0700

    Sign up page

---
+++ src/SignupForm.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./SignUpForm.css";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+function HandleClick(e) {
+  e.preventDefault();
+  alert("Form đã được submit!");
+}
+
+function SignUpForm() {
+  return (
+    <div className="Background">
+    <img className="logo" src="src/image/logo.png"/>
+    <div className="container">
+      <div className="left-container">
+        <h1>EV CHARGING</h1>
+        <h2>Vì niềm tin mang đến trải nghiệm xanh.</h2>
+      </div>
+      <Form className="form-container" onSubmit={HandleClick}>
+        <div className="title">
+          <h1>Đăng ký</h1>
+        </div>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Tên tài khoản</Form.Label>
+          <Form.Control className="placeholdertxt" type="username" placeholder="example123@gmail.com" />
+        </Form.Group>
+        
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Mật khẩu</Form.Label>
+          <Form.Control className="placeholdertxt" type="password" placeholder="Mật khẩu phải ..." />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Xác nhận mật khẩu</Form.Label>
+          <Form.Control className="placeholdertxt" type="password"/>
+        </Form.Group>
+
+
+        <Button variant="primary" type="submit">
+          Đăng ký
+        </Button>
+
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" />
+          <label class="form-check-label">Tôi đồng ý với các điều khoản và dịch vụ.</label>
+        </div>
+
+        <div className="login">
+          <label>Đã có tài khoản? </label><a href=""> Đăng nhập.</a>
+        </div>
+      </Form>
+    </div></div>
+  );
+}
+
+export default SignUpForm;
