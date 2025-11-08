fa2835f7706254d2a13397e74aaff51d0946b34a
TigLe-vn <lenguyenquangt@gmail.com>
2025-11-08 08:31:32 +0700

    refactor: rename Pages to pages (lowercase) for consistency

--- src/Pages/admin/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StationsList } from "./StationsList";
-export { default as UsersList } from "./UsersList";
-export { default as Reports } from "./Reports";
-export { default as AddStation } from "./AddStation";
